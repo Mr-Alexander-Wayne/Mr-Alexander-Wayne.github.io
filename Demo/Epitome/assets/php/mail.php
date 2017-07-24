0ca39d5ef63b6adb434afa6c1325780f1d13b3c2
Mr. Alexander Wayne <araka.ebuka@gmail.com>
2017-07-24 13:05:08 +0100

    edits

--- Demo/Epitome/assets/php/mail.php
+++ Demo/Epitome/assets/php/mail.php
@@ -6,5 +6,5 @@ $recipient = "araka.ebuka@gmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+echo "Thank You!" . " -" . "<a href='form.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
 ?>
