ee5f0071bcf1e40ba715f764ee747c60eb74637d
Mr. Alexander Wayne <araka.ebuka@gmail.com>
2017-12-21 16:58:32 +0100

    000

--- Demo/Modex/ModexLagos/contact.php
+++ Demo/Modex/ModexLagos/contact.php
@@ -1,9 +1,10 @@
 <?php $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
-$formcontent="From: $name \n Message: $message";
-$recipient = "olabiranoluwatoyin@yahoo.com";
-$subject = "You were contacted through the contact form on your website";
+$category = $_POST['category'];
+$formcontent="Hello there, \n You have received an enquiry from your website. \n From: $name \n Message: $message \n\n\n\nPowered by: The Alexander Wayne";
+$recipient = "admin.modexlagos@modexng.net";
+$subject = "Enquiry to Modex Guest-House $category" ;
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 echo "Thank You!";
--- Demo/Modex/ModexLagos/mail.php
+++ Demo/Modex/ModexLagos/mail.php
@@ -1,9 +1,15 @@
-<?php $name = $_POST['name'];
+<?php $fname = $_POST['fname'];
+$lname = $_POST['lname']
 $email = $_POST['email'];
-$message = $_POST['message'];
-$formcontent="From: $name \n Message: $message";
-$recipient = "olabiranoluwatoyin@yahoo.com";
-$subject = "You were contacted through the contact form on your website";
+$tel = $_POST['tel'];
+$category = $_POST['category'];
+$adate = $_POST['adate'];
+$ddate = $_POST['ddate'];
+$formcontent="Hello there, \n You have received a booking request made from your website. 
+\nBooking Summary: $lname $fname would love to book a $category room. He/She would be staying between $adate and $ddate. You can contact him/her on $tel or $email. \n \nKindly find the full details below: \n\n\n
+\nLast Name: $lname \nFirst Name: \n$fname \nEmail: $email \nPhone Number: $tel \nArrival Date: $adate \nDeparture Date: $ddate \nRoom Type: $category \n\n\n\nPowered by: The Alexander Wayne";
+$recipient = "admin.modexlagos@modexng.net";
+$subject = "Booking Application to Modex Guest-House" ;
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 echo "Thank You!";
